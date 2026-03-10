afca2cf921d66686bc6dd560a0fd15f59b6ec088
Malith Ranasinghe <ranasinghemalith20@gmail.com>
2026-03-10 22:02:14 +0530

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next';
 import QueryProvider from '@/providers/QueryProvider';
 import './globals.css';
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: 'Population Ranking Chart',
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <QueryProvider>{children}</QueryProvider>
       </body>
